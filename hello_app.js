66d838215ea2deaa1132727bab1e4b2b1744052d
JJ Stewart <jstewa12@tufts.edu>
2020-04-25 22:17:03 -0400

    killin it

--- hello_app.js
+++ hello_app.js
@@ -1,1 +1,13 @@
-document.write("hello all");
+var http = require('http');
+
+var port = process.env.PORT || 8080;
+http.createServer((request, response) => {
+    if (request.url === '/favicon.ico') {
+        console.log("Fucking favicon");
+        response.end();
+        return;
+    }
+
+    response.write("Hello world!");
+    console.log("Hello world!");
+}).listen(8080);
